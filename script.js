d0915c37122c7c7bad718c07d1839fd2254b7f23
hamed-skyboy <h.miriyan69@gmail.com>
2025-06-10 11:25:33 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("paraphrase-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const inputText = document.getElementById("input-text").value;
-    document.getElementById("result").innerText = "نسخه بازنویسی شده: " + inputText; // شبیه‌سازی بازنویسی
-});
