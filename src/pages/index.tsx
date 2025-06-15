5d71021a5779c2e0c3490c4e44229bd29dff0663
hamed-skyboy <h.miriyan69@gmail.com>
2025-06-15 11:27:45 +0330

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to AI Writing Tools</h1>
+      <p>This is a demo homepage for paraphrasing and summarizing tools.</p>
+    </div>
+  );
+}
